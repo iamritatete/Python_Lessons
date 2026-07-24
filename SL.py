7ae417369e6c861573d41dd06d52f0fcd43dffe9
iamritatete <amritatete220@gmail.com>
2026-07-24 20:44:38 +0530

    second commit

--- SL.py
+++ SL.py
@@ -13,3 +13,4 @@ y=data.iloc[:, 1].values
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+#training the model
